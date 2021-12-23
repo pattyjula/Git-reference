8f708beecf5925fbe47ddd0025414e78b2c621d4
Patty <pattyjula@gmail.com>
2021-12-23 12:07:03 -0800

    Create upload_to_AGOL.py

---
+++ upload_to_AGOL.py
@@ -0,0 +1,7 @@
+'''
+Instructions for loading file to ArcGIS Online
+
+This is a simple, useful, and best-practice example for use with Python 3.7 and ArcGIS Pro 2.9
+
+For security and learning purposes some steps are comments only.
+'''
