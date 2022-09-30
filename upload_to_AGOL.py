536c18f82d7f78041041fb83a110f0271b4ddffe
Patty <pattyjula@gmail.com>
2022-09-30 10:39:15 -0700

    pushing changes

--- upload_to_AGOL.py
+++ upload_to_AGOL.py
@@ -1,43 +1,72 @@
-'''
-Instructions for loading file to ArcGIS Online
+# -*- coding: utf-8 -*-
+"""
+        @Author: Patty Jula
+        @Date created: 3/2022
+        @Date updated: 9/30/2022
+        @Description: This script takes a data wrangled shapefile, converts to 
+        a json
+        and overwrites an existing feature layer on ArcGIS Online.
+        @Note: This runs with Python 3.9.
+"""
 
-This is a simple, useful, and best-practice example for 
-use with Python 3.7 and ArcGIS Pro 2.9 and is 
-provided without warranty of any kind.
-
-For security and learning purposes some steps are comments only.
-'''
-
-import arcpy
-import arcgis
-from arcgis.gis import GIS
 import os
-import zipfile
+import sys
+
+import arcgis
+import arcpy
+from arcgis.gis import GIS
 
 arcpy.env.overwriteOutput = True
 
-# create query layer
-# and include SQL statement
-arcpy.management.MakeQueryLayer(db, "queryLayer", """SELECT OBJECTID, Shape, GDB_GEOMATTR_DATA, value 
-FROM source.file""", "OBJECTID", "POINT", "4326", 'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]];-400 -400 1000000000;-100000 10000;-100000 10000;8.98315284119521E-09;0.001;0.001;IsHighPrecision', "DEFINE_SPATIAL_PROPERTIES", "DO_NOT_INCLUDE_M_VALUES", "DO_NOT_INCLUDE_Z_VALUES", "-119.326209362609 33.3970626359568 -117.090481647225 34.8267769260519")
-
-# export layer
-with arcpy.EnvManager(transferDomains="TRANSFER_DOMAINS"):
-    arcpy.conversion.FeatureClassToShapefile("queryLayer", output_path)
+ws = sys.argv[1]
+loc = ws
+pth = os.chdir(loc)
+output_feature_class = "ARL_data.shp"
+localJSON = ws + "./arl_content.json"
+try:
+    if os.path.isfile(localJSON):
+        os.remove(localJSON)
     
-# create zip file here
-# The variable below will be a path or a path plus the file name
-z = ""
+except FileNotFoundError:
+    print("Wrong file or file path")
 
-# connect to AGOL
-site_internal = GIS(site, un, pw)
+# Truncate the existing feature layer
+def TruncateWebLayer(gis=None, target=None):
+    try:
+        lyr = arcgis.features.FeatureLayer(target, gis)
+        lyr.manager.truncate()
+        print("Successfully truncated layer: " + str(target))
+    except:
+        print("Failed truncating: " + str(target))
+        sys.exit()
 
-# Use shapefile not feature service ID
-shpID = site_internal.content.get('')
+try:
 
-# Call the update method to replace/overwrite it with the zip file from disk
-shpID.update({}, z)
-# Update feature layer
-shpID.publish(overwrite=True)
+    # LAPD log in, create a profile so you don't need to share un/pw
+    mygis = GIS("https://lapd.maps.arcgis.com/", profile=sys.argv[2], verify_cert=False)
 
-print(':)')
+    # publishedWebLayer is the URL of a single feature layer within a collection in
+    # an AGOL portal
+    publishedWebLayer = r"https://services6.arcgis.com/Q18o8KwHjFGbEc4j/arcgis/rest/services/ARL_data/FeatureServer/0"
+
+    # a feature class on the local system with the same schema as the
+    # portal layer
+    updateFeatures = os.path.join("E:/batch/arl/content/" + output_feature_class)
+
+    # remove all features from the already published feature layer
+    TruncateWebLayer(mygis, publishedWebLayer)
+
+    # reference the empty layer as FeatureLayer object from the ArcGIS Python API
+    fl = arcgis.features.FeatureLayer(publishedWebLayer, mygis)
+
+    # create a JSON object from the local features
+    jSON = arcpy.FeaturesToJSON_conversion(updateFeatures, localJSON)
+
+    # create a FeatureSet object from the JSON
+    fs = arcgis.features.FeatureSet.from_json(open(localJSON).read())
+
+    # add/append the local features to the hosted feature layer
+    fl.edit_features(adds=fs)
+
+except arcpy.ExecuteError:
+    print(arcpy.GetMessages())
